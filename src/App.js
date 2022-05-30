c57bba26698b1bb285c1d82784b0854841bd5dcd
pandroza <60747184+pandroza@users.noreply.github.com>
2022-05-30 03:18:20 +0100

    twelveth app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,7 @@ import 'css-doodle';
 
 function App() {
   return (
-    <css-doodle>{`
-      :doodle {
-        @grid: 2 / 200px;
-        grid: 1px;
-      }
-      background: @pick(red, pink);
-    `}</css-doodle>
+    <css-doodle use="var(--rule1)"id = "doodle"></css-doodle>
   );
 }
 
